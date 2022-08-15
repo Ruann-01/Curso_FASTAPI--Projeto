50ba867f7e53d0f4dd6ae8527b77d384fbbb8a8c
Ruann Campos <44266061+Ruann-01@users.noreply.github.com>
2022-08-15 16:29:05 -0300

    Construindo End point Delete

--- secao03/main.py
+++ secao03/main.py
@@ -1,4 +1,5 @@
 from typing import List, Optional
+from fastapi.responses import JSONResponse
 from fastapi import FastAPI
 from fastapi import HTTPException
 from fastapi import status
@@ -44,11 +45,20 @@ async def put_curso(curso_id: int, curso: Curso):
     if curso_id in cursos:
         cursos[curso_id] = curso
         del curso.id
-        
+
         return curso
     else:
         raise HTTPException(
             status_code = status.HTTP_404_NOT_FOUND, detail=f'Não existe curso com o ID {curso_id}')
+
+@app.delete('/cursos/{curso_id}')
+async def delete_curso(curso_id: int):
+    if curso_id in cursos:
+        del cursos[curso_id]
+        return JSONResponse(status_code = status.HTTP_204_NO_CONTENT)
+    else:
+         raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND, detail=f'Não existe curso com o ID {curso_id}')  
 
 if __name__ == '__main__':
     import uvicorn 
